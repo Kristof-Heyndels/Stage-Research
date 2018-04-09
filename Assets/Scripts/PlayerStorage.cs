2285eed48185e654fcf04b6d4ded136cefe2035d
Kristof H <kheyndels@gmail.com>
2018-04-09 15:28:38 +0200

    Basic scene

---
+++ Assets/Scripts/PlayerStorage.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class PlayerStorage : MonoBehaviour
+{
+	public GameObject head;
+	public GameObject body;
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		head.transform.position = Camera.main.transform.position;
+		body.transform.position = new Vector3(head.transform.position.x, head.transform.position.y - 0.75f, head.transform.position.z);
+
+		head.transform.localEulerAngles = new Vector3(Camera.main.transform.localEulerAngles.x, Camera.main.transform.localEulerAngles.y, Camera.main.transform.localEulerAngles.z);
+		body.transform.eulerAngles = Vector3.zero;
+	}
+}
---
+++ Assets/Scripts/World.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class World : MonoBehaviour
+{
+	public GameObject playerStorage;
+
+	public GameObject leftController;
+	public GameObject rightController;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (OVRInput.Get(OVRInput.Axis1D.SecondaryHandTrigger) > 0.8f)
+		{
+
+		}
+
+		if (OVRInput.Get(OVRInput.Axis1D.PrimaryHandTrigger) > 0.8f)
+		{
+
+		}
+	}
+}

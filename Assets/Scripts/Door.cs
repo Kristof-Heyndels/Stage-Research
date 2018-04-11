27c1ab586ebe65e6b04d7940fdf5c661d7dc074a
Kristof H <kheyndels@gmail.com>
2018-04-11 10:57:58 +0200

    Turning implemented

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		if (PlayerStorage.isInit() && transform.position.y < 5)
+		{
+			transform.position = new Vector3(transform.position.x, transform.position.y + 0.002f, transform.position.z);
+		}
+	}
+}
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -1,8 +1,13 @@
-﻿using UnityEngine;
+﻿using System;
+using UnityEngine;
 
 public class World : MonoBehaviour
 {
-	public GameObject playerStorage;
+
+	public GameObject localAvatar;
+
+	private float oldPrimaryStick;
+	private float oldSecondaryStick;
 
 	// Use this for initialization
 	void Start()
@@ -13,6 +18,22 @@ public class World : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
+		Debug.Log(String.Format("Left thumbstick: {0}, Right thumbstick: {1}", OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick), OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick)));
+
+		var turnAngle = 0f;
+
+		if (OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x > 0.8f && oldPrimaryStick < 0.8f)
+		{
+			turnAngle = 30;
+		}else if (oldPrimaryStick > -0.8f && OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x < -0.8f)
+		{
+			turnAngle = -30;
+		}
+
+		oldPrimaryStick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x;
+		oldSecondaryStick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x;
+
+		localAvatar.transform.localEulerAngles = new Vector3(localAvatar.transform.localEulerAngles.x, localAvatar.transform.localEulerAngles.y + turnAngle, localAvatar.transform.localEulerAngles.z);
 
 	}
 }

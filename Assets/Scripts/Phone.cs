f9e3076dfd5fac32f28e1c3e003e2565783e840c
Kristof H <kheyndels@gmail.com>
2018-04-12 14:21:39 +0200

    StickyPhone

--- Assets/Scripts/Phone.cs
+++ Assets/Scripts/Phone.cs
@@ -3,6 +3,7 @@
 public class Phone : MonoBehaviour
 {
 	private float offset;
+	private bool init;
 
 	// Use this for initialization
 	void Start()
@@ -13,39 +14,39 @@ public class Phone : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		offset = GameObject.FindGameObjectWithTag("MainCamera").transform.eulerAngles.y;
+		//offset = GameObject.FindGameObjectWithTag("MainCamera").transform.eulerAngles.y;
 
-		if (PlayerStorage.isInit() && !gameObject.GetComponent<OVRGrabbable>().isGrabbed)
-		{
-			transform.position = PlayerStorage.pocketLocation();
-			transform.eulerAngles = new Vector3(0, 90 + offset, 0);
-		}
+		//if (PlayerStorage.isInit() && !gameObject.GetComponent<OVRGrabbable>().isGrabbed)
+		//{
+		//	transform.position = PlayerStorage.pocketLocation();
+		//	transform.eulerAngles = new Vector3(0, 90 + offset, 0);
+		//}
 	}
 
 	void OnTriggerStay(Collider other)
 	{
-		if (!PlayerStorage.isInit())
-		{
-			if (other.CompareTag("Body") && gameObject.GetComponent<OVRGrabbable>().isGrabbed)
-			{
-				PlayerStorage.pocketEnabled(true);
-				PlayerStorage.movePocket(gameObject);
-			}
-		}
+		//if (!PlayerStorage.isInit())
+		//{
+		//	if (other.CompareTag("Body") && gameObject.GetComponent<OVRGrabbable>().isGrabbed)
+		//	{
+		//		PlayerStorage.pocketEnabled(true);
+		//		PlayerStorage.movePocket(gameObject);
+		//	}
+		//}
 	}
 
 	void OnTriggerExit(Collider other)
 	{
-		if (!PlayerStorage.isInit())
-		{
-			if (other.CompareTag("Body") && gameObject.GetComponent<OVRGrabbable>().isGrabbed)
-			{
-				PlayerStorage.pocketEnabled(false);
-			}
-			else if (other.CompareTag("Pocket") && !gameObject.GetComponent<OVRGrabbable>().isGrabbed)
-			{
-				PlayerStorage.checkInit();
-			}
-		}
+		//if (!PlayerStorage.isInit())
+		//{
+		//	if (other.CompareTag("Body") && gameObject.GetComponent<OVRGrabbable>().isGrabbed)
+		//	{
+		//		PlayerStorage.pocketEnabled(false);
+		//	}
+		//	else if (other.CompareTag("Pocket") && !gameObject.GetComponent<OVRGrabbable>().isGrabbed)
+		//	{
+		//		PlayerStorage.checkInit();
+		//	}
+		//}
 	}
 }
--- Assets/Scripts/World.cs
+++ Assets/Scripts/World.cs
@@ -1,10 +1,21 @@
-﻿using System;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class World : MonoBehaviour
 {
 
+	public enum Handedness
+	{
+		Right,
+		Left
+	}
+
 	public GameObject localAvatar;
+	public GameObject phone;
+
+	public GameObject avatarGrabberLeft;
+	public GameObject avatarGrabberRight;
+
+	public Handedness handedness;
 
 	private float oldPrimaryStick;
 	private float oldSecondaryStick;
@@ -18,28 +29,51 @@ public class World : MonoBehaviour
 	// Update is called once per frame
 	void Update()
 	{
-		var turnAngle = 0f;
+		//NOTE(Kristof): Rotating the avatar with the joysticks
+		{
+			var turnAngle = 0f;
 
-		if (OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x > 0.8f && oldPrimaryStick < 0.8f)
-		{
-			turnAngle = 30;
-		}else if (oldPrimaryStick > -0.8f && OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x < -0.8f)
-		{
-			turnAngle = -30;
+			if (OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x > 0.8f && oldPrimaryStick < 0.8f)
+			{
+				turnAngle = 30;
+			}
+			else if (oldPrimaryStick > -0.8f && OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x < -0.8f)
+			{
+				turnAngle = -30;
+			}
+
+			if (OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x > 0.8f && oldSecondaryStick < 0.8f)
+			{
+				turnAngle = 30;
+			}
+			else if (oldSecondaryStick > -0.8f && OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x < -0.8f)
+			{
+				turnAngle = -30;
+			}
+
+			oldPrimaryStick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x;
+			oldSecondaryStick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x;
+
+			localAvatar.transform.localEulerAngles = new Vector3(localAvatar.transform.localEulerAngles.x,
+				localAvatar.transform.localEulerAngles.y + turnAngle, localAvatar.transform.localEulerAngles.z);
 		}
 
-		if (OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x > 0.8f && oldSecondaryStick < 0.8f)
+		//NOTE(Kristof): Grabbing the phone
 		{
-			turnAngle = 30;
+			if (handedness == Handedness.Right)
+			{
+				//NOTE(Kristof): phone child left hand
+				phone.transform.parent = avatarGrabberLeft.transform.GetChild(0).transform;
+				phone.transform.localPosition = Vector3.zero;
+				phone.transform.localEulerAngles = new Vector3(45, 0, 0);
+			}
+			else
+			{
+				//NOTE(Kristof): phone child right hand
+				phone.transform.parent = avatarGrabberRight.transform.GetChild(0).transform;
+				phone.transform.localPosition = Vector3.zero;
+				phone.transform.localEulerAngles = new Vector3(45, 0, 0);
+			}
 		}
-		else if (oldSecondaryStick > -0.8f && OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x < -0.8f)
-		{
-			turnAngle = -30;
-		}
-
-		oldPrimaryStick = OVRInput.Get(OVRInput.Axis2D.PrimaryThumbstick).x;
-		oldSecondaryStick = OVRInput.Get(OVRInput.Axis2D.SecondaryThumbstick).x;
-
-		localAvatar.transform.localEulerAngles = new Vector3(localAvatar.transform.localEulerAngles.x, localAvatar.transform.localEulerAngles.y + turnAngle, localAvatar.transform.localEulerAngles.z);
 	}
 }

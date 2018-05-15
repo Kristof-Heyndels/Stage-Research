d210f8822aef8234432b42d37ec2d9a998009c78
lg188 <lander.ghekiere@gmail.com>
2018-05-15 11:06:43 +0200

    Added second demo room
    added 3x3 grid of spheres
    attempt at making sphere highlightable

---
+++ Assets/Scripts/Interactables/PatternLock.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PatternLock : MonoBehaviour
+{
+
+	public int gridSize;
+	public List<GameObject> spheresRaw;
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+}
---
+++ Assets/Scripts/Interactables/PatternSphere.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using VRTK;
+
+public class PatternSphere : MonoBehaviour
+{
+
+
+	// Use this for initialization
+	void Start()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+
+	}
+
+	public void StartUsing(VRTK_InteractUse currentUsingObject = null)
+	{
+		Debug.LogFormat("Interacted: {0}", currentUsingObject);
+	}
+}

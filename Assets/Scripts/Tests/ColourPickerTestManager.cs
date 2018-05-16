d23c100e39aeebf0e0e16f3f1d7bb208f5c61687
Kristof H <kheyndels@gmail.com>
2018-05-16 08:55:56 +0200

    dropdown test finished

--- Assets/Scripts/Tests/ColourPickerTestManager.cs
+++ Assets/Scripts/Tests/ColourPickerTestManager.cs
@@ -4,11 +4,16 @@ using UnityEngine.UI;
 
 public class TestTimer
 {
-	public float timer;
+	public float Time;
 
-	public void Next(float time)
+	public void Next(float deltaTime)
 	{
-		timer += time;
+		this.Time += deltaTime;
+	}
+
+	public void Reset()
+	{
+		Time = 0;
 	}
 }
 
@@ -20,22 +25,51 @@ public class ColourPickerTestManager : MonoBehaviour
 	public Text infoText;
 
 	private Color colourToSelect;
+
+	private TestTimer timer = new TestTimer();
 	private float[] dropdownTimers = new float[5];
 	private float[] colourCubeTimers = new float[5];
+	private int i = 0;
 
-	static System.Random rnd =  new System.Random();
+	private bool dropTest;
+	private bool preparingNext;
+	private bool cubeTest;
+
+	static System.Random rnd = new System.Random();
 
 	// Update is called once per frame
-		void Update()
+	void Update()
 	{
-		if (!holoPanel.activeSelf)
+		if (dropTest || cubeTest)
 		{
-			infoText.text = "You may apologise for your short attention span, but please Open your Holo";
+			timer.Next(Time.deltaTime);
+		}
+
+		if (preparingNext)
+		{
+			if (!holoPanel.activeSelf)
+			{
+				infoText.text = "Engage the holographic cube to begin the next stage.";
+			}
+		}
+
+		if (dropTest)
+		{
+			if (!holoPanel.activeSelf)
+			{
+				infoText.text = "You may apologise for your short attention span, but please Open your Holo";
+			}
+		}
+		else if (cubeTest)
+		{
+
 		}
 	}
 
 	public void BeginTest()
 	{
+		dropTest = true;
+
 		var colours = new List<string>
 		{
 			"White",
@@ -56,6 +90,10 @@ public class ColourPickerTestManager : MonoBehaviour
 
 		colourToSelect = ColourRGBA.magenta;
 		HoloPanel.LogNotification("Starting test: COLOUR PICKER\nPlease select the colour: {0}", ColourRGBA.ToName(colourToSelect));
+	}
+
+	public void NextTest()
+	{
 		
 	}
 
@@ -66,9 +104,38 @@ public class ColourPickerTestManager : MonoBehaviour
 
 		if (selectedColour == colourToSelect)
 		{
-			colourToSelect = ColourRGBA.colourList[rnd.Next(ColourRGBA.colourList.Count)];
-			HoloPanel.LogNotification("Impressive for one of your kind.\nPlease select the next colour: {0}", ColourRGBA.ToName(colourToSelect));
+			if (dropTest)
+			{
+				dropdownTimers[i] = timer.Time;
+				i++;
+			}
+			else if (cubeTest)
+			{
+				colourCubeTimers[i] = timer.Time;
+				i++;
+			}
+			timer.Reset();
+
+			if (i == 5)
+			{
+				if (dropTest)
+				{
+					dropTest = false;
+					preparingNext = true;
+					i = 0;
+					HoloPanel.LogNotification("Moving to next stage: COLOUR CUBES\nPlease close your Holo to continue.", ColourRGBA.ToName(colourToSelect));
+				}
+				else if (cubeTest)
+				{
+					//NOTE(Kristof): Finish this test
+				}
+			}
+			else
+			{
+				colourToSelect = ColourRGBA.colourList[rnd.Next(ColourRGBA.colourList.Count)];
+				HoloPanel.LogNotification("Impressive for one of your kind.\nPlease select the next colour: {0}", ColourRGBA.ToName(colourToSelect));
+			}
 		}
 	}
-	
+
 }

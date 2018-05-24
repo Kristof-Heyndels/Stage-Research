f898a31175578035a481dc8e1fac91d0231aab8b
lg188 <lander.ghekiere@gmail.com>
2018-05-24 08:17:13 +0200

    Only select colour from drop on trigger down

--- Assets/Scripts/HoloUI/DropItem.cs
+++ Assets/Scripts/HoloUI/DropItem.cs
@@ -3,10 +3,24 @@
 public class DropItem : HoloUI
 {
 	public Dropdown dropdown;
+
+	private float con1;
+	private float con2;
+
 	public override void Execute()
 	{
-		gameObject.GetComponent<Toggle>().Select();
-		dropdown.value = transform.GetSiblingIndex() - 1;
-		dropdown.gameObject.GetComponent<ColourDropdown>().ColourSelected();
+		var con1 = OVRInput.Get(OVRInput.Axis1D.PrimaryIndexTrigger);
+		var con2 = OVRInput.Get(OVRInput.Axis1D.SecondaryIndexTrigger);
+
+		if (con1 < 0.5f && 0.5f < this.con1 || con2 < 0.5f && 0.5f < this.con2)
+		{
+			gameObject.GetComponent<Toggle>().Select();
+			dropdown.value = transform.GetSiblingIndex() - 1;
+			dropdown.gameObject.GetComponent<ColourDropdown>().ColourSelected();
+		}
+
+		this.con1 = con1;
+		this.con2 = con2;
+
 	}
 }
